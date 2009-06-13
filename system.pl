6de116864684330d9ce82ba297b1e747a89af17d
raceimaztion <dvanhumb@sfu.ca>
2009-06-13 01:36:08 -0700

    "How many" and "what" questions now work with the same code, changed "improper nouns" into "who words" to more accurately control meaning, and improved the solver and its memory. We probably can remove the query predicates, as most of the work is done by the fragment predicates right now.

--- system.pl
+++ system.pl
@@ -1,7 +1,7 @@
 % This requires Hyprolog
 %:-consult(hyprolog).
-assumptions ref/1, proof/1, given_true/1, given_false/1, count/1, missing/1.
-dynamic 
+assumptions ref/1, proof/1, count/1, missing/1.
+:- dynamic given_true/1, given_false/1.
 
 % ====== Database: ======
 disease(X):-member(X, [cold, influenza, malaria, measles, mumps]).
@@ -27,6 +27,9 @@ has(john, cold).
 has(D, P):-doctor(D),patient(P).
 has(P, D):-patient(P),doctor(D).
 has(D, N):-doctor(D),nurse(N).
+have(A, B):-has(A, B);had(A, B).
+
+are_sick(P):- (have(P,X);had(P,X)),disease(X).
 
 % Symptoms list
 symptom_of(cold, S):-member(S, [coughing, nasal_congestion, runny_nose, sore_throat]).
@@ -50,6 +53,7 @@ rule(problem(malaria), [anemia, fever, shivering, vomiting]).
 rule(problem(measles), [coughing, runny_nose, red_eyes, red_rash]).
 rule(problem(mumps), [fever, headache, swelling]).
 
+hot(_).
 bad(_).
 sick(_).
 high(_).
@@ -62,12 +66,12 @@ the(_,A,B):-call(A),call(B).
 a(_,A,B):-call(A),call(B).
 defined_is(A,A).
 and(A, B) :- call(A), call(B).
-how_many(X, M) :- setof(X, M, Result), length(Result, Length), +count(Length).
+how_many(X, M, Length) :- setof(X, M, Result), length(Result, Length).%, +count(Length).
 
 % Useful display predicates:
 display_list([], _) :- nl.
 display_list([X], _) :- write(X), write('.'), nl, !.
-display_list([X, Y], L) :- write(X), (L<3;write(',')), write(' and '), write(Y), nl,!.
+display_list([X, Y], L) :- write(X), (L<3;write(',')), write(' and '), write(Y), write('.'), nl,!.
 display_list([X|List], L) :- write(X), write(', '), display_list(List, L).
 display_list(List) :- length(List, Length), display_list(List, Length).
 
@@ -82,7 +86,8 @@ preposition('FIRST') --> [].
 preposition(X) --> [X],{preposition(X)}.
 preposition(X):-member(X,[about, above, across, after, against, along, among, around, at, before, behind, below, beneath, beside, between, beyond, but, by, despite, down, during, except, for, from, in, inside, into, like, near, of, off, on, onto, out, outside, over, past, since, through, throughout, till, to, toward, under, underneath, until, up, upon, with, within, without]).
 
-query(setof(X, Meaning, Results)) --> [what],[are],fragment(X, Meaning),['?'].
+query(Problem) --> [what,is,the,problem,'?'],{solve(problem(Problem))}.
+query(setof(X, Meaning, Results)) --> [what,are],fragment(X, Meaning),['?'],{display_list(Results)}.
 
 % Sentence fragment:
 fragment(Subject, Meaning) --> nounPhrase(Subject, _, Meaning).
@@ -106,24 +111,35 @@ verb(Subject, got(Subject, Object, Subject2), [['FIRST', Object], [from, Subject
 verb(Subject, gave(Subject, Object, Subject2), [['FIRST', Object], [to, Subject2]]) --> [gave].
 verb(Subject, symptom_of(Object, Subject), [['FIRST', Object]]) --> [symptom], [of].
 verb(Subject, symptom_of(Object, Subject), [['FIRST', Object]]) --> [symptoms], [of].
+verb(Subject, are_sick(Subject), []) --> [are,sick];[are,ill].
 
 % Noun phrase:
 nounPhrase(Subject, Meaning, Meaning) --> properNoun(Subject).
-nounPhrase(Subject, Meaning, Meaning) --> whWord(Subject).
-nounPhrase(Subject, VerbPhrase, Meaning) --> determinant(Subject, AdjPhrase, VerbPhrase, Meaning), adjective(Subject, NounPhrase, AdjPhrase), commonNoun(Subject, Noun), relativeClause(Subject, Noun, NounPhrase).
+nounPhrase(Subject, VerbPhrase, Meaning) --> whWord(Subject, NounPhrase, Meaning), nounPhrase(Subject, VerbPhrase, NounPhrase), !.
+nounPhrase(Subject, VerbPhrase, Meaning) --> whWord(Subject, VerbPhrase, Meaning).
+nounPhrase(Subject, VerbPhrase, Meaning) --> determiner(Subject, AdjPhrase, VerbPhrase, Meaning), adjective(Subject, NounPhrase, AdjPhrase), commonNoun(Subject, Noun), relativeClause(Subject, Noun, NounPhrase).
 
 nounPhrase(Subject, Meaning, Meaning) --> {-missing(Subject)}.
 
-
 % Proper nouns: (names)
-properNoun(X) --> [X],{disease(X);symptom(X);name(X);member(X,[medicine, cure, symptom, symptoms])}.
+properNoun(X) --> [X],{disease(X);symptom(X);name(X);member(X,[medicine, cure])}.
 
-% "Improper" nouns:
-whWord(Who) --> [who],{+ref(Who)}.
-whWord(He) --> [he],{+ref(He)}.
-whWord(She) --> [she],{+ref(She)}.
-whWord(What) --> [what],{+ref(What)}.
-%whWord(HowMany) --> [how],[many],{+ref(HowMany)}.
+% Who nouns:
+whWord(Who,M,M) --> [who],{+ref(Who)}.
+whWord(What,M,M) --> [what],{+ref(What)}.
+whWord(What,M,M) --> [what,are], {+ref(What)}.
+whWord(People,M,M) --> [people], {+ref(People)}.
+
+whWord(Subject,VP,and(disease(Subject), VP)) --> [disease],{+ref(Subject)}.
+whWord(Subject,VP,and(disease(Subject), VP)) --> [diseases],{+ref(Subject)}.
+whWord(Subject,VP,and(symptom(Subject), VP)) --> [symptom],{+ref(Subject)}.
+whWord(Subject,VP,and(symptom(Subject), VP)) --> [symptoms],{+ref(Subject)}.
+
+whWord(He, VerbPhrase, and(he(He),VerbPhrase)) --> [he],{+ref(He)}.
+whWord(She, VerbPhrase, and(she(She), VerbPhrase)) --> [she],{+ref(She)}.
+
+whWord(Subject, VerbPhrase, how_many(Subject, Meaning, C)) --> [how,many], nounPhrase(Subject, VerbPhrase, Meaning), {+count(C)}.
+whWord(HowMany, VerbPhrase, how_many(HowMany, VerbPhrase, C)) --> [how,many], {+count(C)}.
 
 % Common nouns:
 commonNoun(X, flu(X)) --> [flu].
@@ -133,8 +149,9 @@ commonNoun(X, mumps(X)) --> [mumps].
 commonNoun(X, doctor(X)) --> [doctor].
 commonNoun(X, patient(X)) --> [patient].
 commonNoun(X, nurse(X)) --> [nurse].
-commonNoun(X, solve(problem(X), [], Proof)) --> [problem], {+proof(Proof)}.
+commonNoun(X, solve(problem(X))) --> [problem].
 commonNoun(X, symptom(X)) --> [symptoms].
+commonNoun(X, disease(X)) --> [disease];[diseases].
 
 % Plural common nouns:
 pluralCommonNoun(X, disease(X)) --> [diseases].
@@ -142,10 +159,10 @@ pluralCommonNoun(X, doctor(X)) --> [doctors].
 pluralCommonNoun(X, patient(X)) --> [patients].
 pluralCommonNoun(X, nurse(X)) --> [nurses].
 
-% Determinants:
-determinant(Subject, NounPhrase, VerbPhrase, the(Subject, NounPhrase, VerbPhrase)) --> [the].
-determinant(Subject, NounPhrase, VerbPhrase, a(Subject, NounPhrase, VerbPhrase)) --> [a].
-determinant(Subject, NounPhrase, VerbPhrase, of(Subject, NounPhrase, VerbPhrase)) --> [of].
+% Determiners:
+determiner(Subject, NounPhrase, VerbPhrase, the(Subject, NounPhrase, VerbPhrase)) --> [the].
+determiner(Subject, NounPhrase, VerbPhrase, a(Subject, NounPhrase, VerbPhrase)) --> [a].
+determiner(Subject, NounPhrase, VerbPhrase, of(Subject, NounPhrase, VerbPhrase)) --> [of].
 
 % Relative clauses:
 relativeClause(Subject, Noun, and(Noun, R)) --> [of],{+missing(Subject)}, fragment(_, R).
@@ -156,28 +173,32 @@ adjective(_, NounPhrase, NounPhrase) --> [].
 adjective(Subject, NounPhrase, and(NounPhrase, Meaning)) --> [Adj], {member(Adj,[hot,high,sick,bad]), Meaning=..[Adj,Subject]}.
 adjective(Subject, NounPhrase, and(NounPhrase, Meaning)) --> [Adj], {member(Adj,[hot,high,sick,bad]), AdjPhrase=..[Adj,Subject]}, adjective(Subject, AdjPhrase, Meaning).
 
-% Solver for answering queries
+% Solver for answering queries: starter:
+solve(A) :- retractall(given_true(_)), retractall(given_false(_)),
+            write('Please answer yes or no to the following list of symptoms:'), nl,
+            solve(A, [], _).
+
 % Normal solver
 solve(true, _, true) :- !.
 solve(not(A), Rules, not(ProofA)) :- not(solve(A, Rules, ProofA)).
 solve((A, B), Rules, (ProofA, ProofB)) :- !, solve(A, Rules, ProofA), solve(B, Rules, ProofB).
 solve(A, Rules, (A:-ProofB)) :- clause(A, B), solve(B, [(A:-B)|Rules], ProofB).
+
 % Check if we know about this already
-solve(A, _, (A:-given)) :- (-given_true(A)), write('Already given: '), write(A), nl.
-solve(A, _, _) :- -given_false(A),+given_false(A), !, fail.
+solve(A, _, (A:-given)) :- given_true(A), !, write('Symptom already listed: '), write(A), nl.
+solve(A, _, _) :- given_false(A), !, write('Symptom already denied: '), write(A), nl, fail.
 
 % Otherwise, ask the user
-solve(A, Rules, (A:-given)) :- askable(A), ((ask_user(A, Rules), *given_true(A), write('Given as true: '), write(A), nl, !);
-                                            (*given_false(A), write('Given as not true: '), write(A), nl, !, fail)).
+solve(A, Rules, (A:-given)) :- askable(A), ((ask_user(A, Rules), assert(given_true(A)), write('Given as true: '), write(A), nl, !);
+                                            (assert(given_false(A)), write('Given as false: '), write(A), nl, !, fail)).
 
-ask_user(A, Rules):- write(A), write('? Enter true if the goal is true, false otherwise'), nl,
+ask_user(A, Rules):- write(A), write('?'), nl,
                     read(Answer), ask_respond(Answer, A, Rules).
 ask_respond(true, _, _).
 ask_respond(why, A, [Rule|Rules]):- write(Rule), nl, ask_user(A, Rules).
 ask_respond(why, A, []):- ask_user(A, []).
 
 % Responder:
-%respond(how_many(_, _)) :- -ref(
-respond(Meaning):- -ref(X), setof(X, Meaning, Result), display_list(Result).
-%respond(Meaning):- -count(C), write(C), nl.
+respond(Meaning) :- call(Meaning), -count(C), write(C), write('.'), nl, !.
+respond(Meaning) :- -ref(X), setof(X, Meaning, Result), display_list(Result), !.
 
